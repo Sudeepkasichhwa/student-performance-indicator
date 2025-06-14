58b88ecf3e5cc2da92b82ed32894b9055a3092e6
SudeepKasichhwa <casichhwasuzaan16@gmail.com>
2025-06-14 12:47:56 +0545

    Create custom exception handling

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_detail(error, error_detail):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = f"Error occurred in script: [{file_name}] at line number: {exc_tb.tb_lineno} error-message: {str(error)}"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message
